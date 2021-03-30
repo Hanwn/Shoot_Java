6fbf5270924ec6aac669360d19fd313d43601287
Hanwn <hanwn7721@gmail.com>
2021-03-30 19:05:20 +0800

    :bug: fix a bug

---
+++ src/Respond/HeadMethod.java
@@ -0,0 +1,12 @@
+package Respond;
+/** 
+ * projectName: Shoot 
+ * fileName: HeadMethod 
+ * packageName: Respond 
+ * author: Hanwn
+ * date: 2021/3/17 20:50 
+ * github: https://www.github.com/Hanwn
+ */
+
+public class HeadMethod {
+}
---
+++ src/Respond/PostMethod.java
@@ -0,0 +1,18 @@
+package Respond;
+
+/**
+ * projectName: Shoot
+ * fileName: PostMethod
+ * packageName: Respond
+ * author: Hanwn
+ * date: 2021/3/17 10:45
+ * github: https://www.github.com/Hanwn
+ */
+
+public class PostMethod {
+
+    static public String send() {
+        return "";
+    }
+
+}
